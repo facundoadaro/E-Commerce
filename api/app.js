8271dd813290e4678d7e911c8a52bdadb3cacdb7
Facu Adaro <facundoadaro0@gmail.com>
2022-08-17 15:53:16 -0300

    commit 17-8

--- api/app.js
+++ api/app.js
@@ -2,22 +2,24 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
 
 const app = express();
-
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use("/", indexRouter);
 
 app.listen(8080, () => {
-    console.log("The server is now running on Port 8080"); // eslint-disable-line no-console
-  });
+  console.log("The server is now running on Port 8080"); // eslint-disable-line no-console
+});
 
 module.exports = app;
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 require("dotenv").config();
 
@@ -9,9 +9,29 @@ mercadopago.configure({
   access_token: process.env.ACCESS_TOKEN,
 });
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.status(200).send('Anda piola loco')
+router.post("/checkout", function (req, res) {
+  // Crea un objeto de preferencia
+  let preference = {
+    items: [
+      {
+        title: "Mi producto",
+        unit_price: 100,
+        quantity: 1,
+      },
+    ],
+  };
+
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      // En esta instancia deberás asignar el valor dentro de response.body.id por el ID de preferencia solicitado en el siguiente paso
+
+      res.redirect(response.body.init_point);
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
 module.exports = router;
